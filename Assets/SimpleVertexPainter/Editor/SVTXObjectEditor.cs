daca7268f96f07949cbd45e6bb7f48ca1dad62b2
gangtiejixie <592831273@qq.com>
2020-09-22 21:57:05 +0800

    添加距离测算 以及unity软件包规范文件

--- Assets/SimpleVertexPainter/Editor/SVTXObjectEditor.cs
+++ Assets/SimpleVertexPainter/Editor/SVTXObjectEditor.cs
@@ -9,12 +9,30 @@ namespace SVTXPainterEditor
     [CustomEditor(typeof(SVTXObject))]
     public class SVTXObjectEditor : Editor
     {
+        private string fbxname = "fbx";
+        public GameObject[] meshObjs;
         public override void OnInspectorGUI()
         {
             if (GUILayout.Button("Lauch Vertex Painter"))
                 SVTXPainterWindow.LauchVertexPainter();
 
+
+
+            if (GUILayout.Button("测算顶点距离中心距离"))
+            {
+                (target as SVTXObject).SetVertexDir();
+            }
+
+            //if (GUILayout.Button("FBXExporter"))
+            //{
+            //    meshObjs = new GameObject[1];
+            //    meshObjs[0] = (target as SVTXObject).gameObject;
+
+            //    FBXExporter.ExportFBX("", fbxname, meshObjs, true);
+            //}
             EditorGUILayout.Space();
+
+            base.OnInspectorGUI();
         }
     }
 }
--- Assets/SimpleVertexPainter/Runtime/SVTXObject.cs
+++ Assets/SimpleVertexPainter/Runtime/SVTXObject.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using System.Diagnostics;
+using System.Linq;
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
@@ -26,7 +28,7 @@ namespace SVTXPainter
         {
 
             var mesh = SVTXPainterUtils.GetMesh(gameObject);
-            if(mesh != null)
+            if (mesh != null)
             {
                 m_record.index = m_historyIndex;
                 m_historyIndex++;
@@ -41,7 +43,8 @@ namespace SVTXPainter
         public void OnUndoRedo()
         {
             var mesh = SVTXPainterUtils.GetMesh(gameObject);
-            if (mesh == null) {
+            if (mesh == null)
+            {
                 return;
             }
             if (m_historyIndex != m_record.index)
@@ -66,6 +69,57 @@ namespace SVTXPainter
             UnityEditor.Undo.undoRedoPerformed -= OnUndoRedo;
             UnityEditorInternal.InternalEditorUtility.RepaintAllViews();
         }
+
+
+        public float m_fMaxDistance = 1.0f;
+        public Vector3 m_vecPositionOffset = Vector3.zero;
+
+        private void OnDrawGizmos()
+        {
+
+            Gizmos.color = Color.blue;
+            Gizmos.DrawLine(transform.position +m_vecPositionOffset, transform.localPosition + m_vecPositionOffset + Vector3.one * m_fMaxDistance);
+            Gizmos.color = Color.red;
+            Gizmos.DrawSphere(transform.position + m_vecPositionOffset, 1);
+
+            
+
+        }
+
+        public void SetVertexDir()
+        {
+            MeshFilter pMeshFilter = GetComponent<MeshFilter>();
+            Mesh pMesh = pMeshFilter.sharedMesh;
+            List<Color> colors = pMesh.colors.ToList();
+            //colors =  ;// new Color[pMesh.vertices.Length];
+            float pDis = 0;
+            for (int i = 0; i < pMesh.vertices.Length; i++)
+            {
+                colors[i] = Color.black;
+                float dis = Vector3.Distance(pMesh.vertices[i], transform.localPosition + m_vecPositionOffset);
+                if (pDis < dis)
+                {
+                    pDis = dis;
+                }
+            }
+            m_fMaxDistance = pDis;
+            UnityEngine.Debug.Log("Max  dis:"+pDis);
+
+            for (int i = 0; i < pMesh.vertices.Length; i++)
+            {
+                //colors[i] = Color.black;
+                Color temp = pMesh.colors[i];
+               // UnityEngine.Debug.Log("R:" + pMesh.colors[i].r);
+                float dis = Vector3.Distance(pMesh.vertices[i], transform.localPosition + m_vecPositionOffset);
+                temp.g = Mathf.Abs(dis) / m_fMaxDistance;
+                colors[i] = temp;
+                //UnityEngine.Debug.LogFormat("dis:{0}size:{1}",dis, colors[i].g);
+            }
+            pMesh.SetColors(colors);
+            pMeshFilter.sharedMesh = pMesh;
+        }
+
+
 #endif
     }
 }
